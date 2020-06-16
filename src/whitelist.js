94f277e8179ed099aea59522927288d2d2a18d28
David Mohs <dmohs@broadinstitute.org>
2020-06-16 15:10:23 -0400

    Include AoU origins in whitelist.

--- src/whitelist.js
+++ src/whitelist.js
@@ -1,3 +1,9 @@
 module.exports = [
-  'https://broad-shibboleth-prod.appspot.com'
+  'https://broad-shibboleth-prod.appspot.com',
+  'https://all-of-us-workbench-test.appspot.com',
+  'https://all-of-us-rw-staging.appspot.com',
+  'https://all-of-us-rw-stable.appspot.com',
+  'https://all-of-us-rw-perf.appspot.com',
+  'https://all-of-us-rw-preprod.appspot.com',
+  'https://workbench.researchallofus.org'
 ]
