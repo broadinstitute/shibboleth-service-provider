18b19c6e02ad003f2e2f0beefd3159ce30cf26ed
Greg Rushton <grushton@broadinstitute.org>
2020-06-25 14:45:22 -0400

    add duos urls to whitelist

--- src/whitelist.js
+++ src/whitelist.js
@@ -6,5 +6,8 @@ module.exports = [
   'https://all-of-us-rw-perf.appspot.com',
   'https://all-of-us-rw-preprod.appspot.com',
   'https://workbench.researchallofus.org',
-  'https://app.terra.bio'
+  'https://app.terra.bio',
+  'https://duos.broadinstitute.org',
+  'https://duos.dsp-duos-staging.broadinstitute.org',
+  'https://duos.dsp-duos-dev.broadinstitute.org'
 ]
